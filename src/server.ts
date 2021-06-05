8c13569a935c16410bb7d63c151e5a33e02fa7ae
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-06-05 16:53:25 -0300

    routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import express from 'express';
-
+import { categoriesRoutes } from './routes/categories.routes';
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: 'hello world',
-  });
-});
-
+app.use(express.json());
+app.use(categoriesRoutes);
 app.listen(8080, () => console.log('server on'));
