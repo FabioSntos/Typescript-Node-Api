c02c7f526f7406819e38a85704bd7dcfa461940f
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-06-07 18:03:58 -0300

    uuid

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { categoriesRoutes } from './routes/categories.routes';
 const app = express();
 
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 app.listen(8080, () => console.log('server on'));
